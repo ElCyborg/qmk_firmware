02ee58acc4fef6e4422e60a858aede9c15d61993
Ryan Hoeck <git@hoeck.dev>
2025-10-12 02:13:40 -0400

    keyboard.json migration

--- keyboards/yunzii/al68v2/mk637/rules.mk
+++ keyboards/yunzii/al68v2/mk637/rules.mk
@@ -3,7 +3,6 @@
 UART_DRIVER_REQUIRED = yes
 MCU_LDSCRIPT = STM32F103xB
 DEFERRED_EXEC_ENABLE = yes
-ENCODER_ENABLE = yes
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
